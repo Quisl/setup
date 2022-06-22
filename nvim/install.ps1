44516da022bd20f4ed3dced88f4b42444f1b4ea3
Jonas Rabe <quigonjinn1@hotmail.com>
2022-06-22 22:44:41 +0200

    Update install.ps1

--- nvim/install.ps1
+++ nvim/install.ps1
@@ -1,5 +1,7 @@
 # Install Neovim
 choco install neovim
+# Install pynvim
+pip install pynvim
 # Install plug vim
 iwr -useb https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim |`
     ni $HOME/vimfiles/autoload/plug.vim -Force
