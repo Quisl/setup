886474fe8a617f0f574b6f21c78506a38f129d5b
Jonas Rabe <quigonjinn1@hotmail.com>
2022-06-22 22:20:33 +0200

    Added install file for nvim

---
+++ nvim/install.ps1
@@ -0,0 +1,9 @@
+# Install Neovim
+choco install neovim
+# Install plug vim
+iwr -useb https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim |`
+    ni $HOME/vimfiles/autoload/plug.vim -Force
+# Install Lua
+choco install lua53
+# Install Ripgrep (for search within Telescope (vim Plugin))
+choco install ripgrep
